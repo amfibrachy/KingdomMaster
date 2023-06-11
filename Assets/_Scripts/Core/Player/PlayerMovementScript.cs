4334228a1b99b227b9ee8685a831842faca6da43
saksarch@gmail.com <amfibrachy>
2023-06-11 21:43:15 +0400

    Added new input system. Remade character movemnt using new system.

--- Assets/_Scripts/Core/Player/PlayerMovementScript.cs
+++ Assets/_Scripts/Core/Player/PlayerMovementScript.cs
@@ -1,7 +1,10 @@
 namespace _Scripts.Core.Player
 {
+    using System;
     using Animations;
+    using InputActions;
     using UnityEngine;
+    using UnityEngine.InputSystem;
     using Utils.Debugging;
 
     public class PlayerMovementScript : MonoBehaviour
@@ -10,34 +13,57 @@ namespace _Scripts.Core.Player
         [SerializeField] private float _speed = 5f;
 
         private float _currentDirection;
+        
+        // Input System
+        private InputActions _input;
+        private InputAction _moveAction;
+        
+        private void Awake()
+        {
+            _input = new InputActions();
+            _moveAction = _input.Player.Move;
+  
+            SubscribeListeners();
+        }
+
+        private void OnEnable()
+        {
+            _moveAction.Enable();
+        }
+
+        public void OnDisable()
+        {
+            _moveAction.Disable();
+        }
+
+        private void SubscribeListeners()
+        {
+        }
 
         private void Update()
         {
-            _currentDirection = Input.GetAxisRaw("Horizontal");
+            _currentDirection = _moveAction.ReadValue<float>();
 
             if (_currentDirection > 0)
             {
-                transform.localScale = new Vector3(1,1,1);
+                transform.localScale = new Vector3(1, 1, 1);
                 _playerAnimation.SetState(_playerAnimation.Walk);
 
-                CDebug.Log("Moving Right");
+                CDebug.Log("Moving Right: " + _currentDirection);
             }
             else if (_currentDirection < 0)
             {
-                transform.localScale = new Vector3(-1,1,1);
+                transform.localScale = new Vector3(-1, 1, 1);
                 _playerAnimation.SetState(_playerAnimation.Walk);
-                
-                CDebug.Log("Moving Left");
+
+                CDebug.Log("Moving Left: " + _currentDirection);
             }
             else
             {
                 _playerAnimation.SetState(_playerAnimation.Idle);
-                CDebug.Log("Stopped moving");
+                CDebug.Log("Stopped moving: " + _currentDirection);
             }
-        }
-
-        private void LateUpdate()
-        {
+            
             transform.Translate(_currentDirection * _speed * Time.deltaTime, 0, 0, Space.World);
         }
     }
