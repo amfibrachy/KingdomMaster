989246f401593e42f207660a77098ee24c6ea857
Amfibrachy <saksarch@gmail.com>
2024-02-15 20:46:48 +0400

    basic UI animations added

---
+++ Assets/_Scripts/Core/BuildSystem/UI/BuildSystemBuildingButtonsPanelScript.cs
@@ -0,0 +1,22 @@
+﻿namespace _Scripts.Core.BuildSystem.UI
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    public class BuildSystemBuildingButtonsPanelScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public event Action OnBuildingPanelHovered;
+        public event Action OnBuildingPanelExit;
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            OnBuildingPanelHovered?.Invoke();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            OnBuildingPanelExit?.Invoke();
+        }
+    }
+}
--- Assets/_Scripts/Core/BuildSystem/UI/BuildSystemInfoScript.cs
+++ Assets/_Scripts/Core/BuildSystem/UI/BuildSystemInfoScript.cs
@@ -1,9 +1,43 @@
 ﻿namespace _Scripts.Core.BuildSystem.UI
 {
+    using DG.Tweening;
     using UnityEngine;
 
     public class BuildSystemInfoScript : MonoBehaviour
     {
+        [Header("Info panel")]
+        [SerializeField] private CanvasGroup _buildingSystemInfoCanvasGroup;
+        [SerializeField] private RectTransform _infoPanelTransform;
+        [SerializeField] private float _initialPosY = -841;
         
+        [Header("Cost panel")] 
+        [SerializeField] private BuildSystemCostScript _buildSystemCost;
+        
+        public bool IsShown { private set; get; }
+        
+        public void ShowPanel()
+        {
+            _buildingSystemInfoCanvasGroup.DOFade(1f, 0.15f);
+            _infoPanelTransform.DOAnchorPosY(0f, 0.3f).OnComplete(() =>
+            {
+                IsShown = true;
+                _buildSystemCost.ShowPanel();
+            });
+        }
+
+        public void HidePanel()
+        {
+            _buildingSystemInfoCanvasGroup.DOFade(0f, 0.15f);
+            _infoPanelTransform.DOAnchorPosY(_initialPosY, 0.3f).OnComplete(() =>
+            {
+                IsShown = false;
+                _buildSystemCost.HidePanel();
+            });
+        }
+
+        public void UpdateInfo(BuildingDataSO data)
+        {
+            Debug.Log("Updated info");
+        }
     }
 }
